7ee31b3295465aab7303028de73873df73d671b6
Your Namebearfoottrader@gmail.com <you@example.com>
2022-06-23 15:15:04 +0100

    updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -49,8 +49,8 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'WeareAllGoingtoMakeit';
+const CONTRACT_SYMBOL = 'WAGMI';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
 const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
